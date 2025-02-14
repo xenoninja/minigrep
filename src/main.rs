7f1495116f4bf18efba0719ade7bc1db43f06c8b
xenoninja <mgotzeus7@gmail.com>
2025-02-14 10:34:55 +0800

    read file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use std::env;
+use std::fs;
 
 fn main() {
+    // parse args
     let pattern = env::args().nth(1).expect("no pattern given");
     let path = env::args().nth(2).expect("no path given");
 
     println!("Searching for '{}'", pattern);
     println!("In file '{}'", path);
+
+    // read file
+    let content = fs::read_to_string(path).expect("could not read file");
+    println!("With text:\n{}", content);
 }
