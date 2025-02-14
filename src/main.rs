310f181b978eaa0e510f188ed10fe83dc7c64fd3
xenoninja <mgotzeus7@gmail.com>
2025-02-14 10:11:02 +0800

    parse the input args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = env::args().nth(1).expect("no pattern given");
+    let path = env::args().nth(2).expect("no path given");
+
+    println!("Searching for '{}'", pattern);
+    println!("In file '{}'", path);
 }
